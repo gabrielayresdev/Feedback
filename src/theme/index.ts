da6764b099e3cee7334dff9ecfe16fca6763fe68
gabrielayresdev <gabriel.ar.fort@gmail.com>
2024-06-23 12:11:51 -0300

    feat: project theme

---
+++ src/theme/index.ts
@@ -0,0 +1,23 @@
+export default {
+  SIZE: {
+    XXS: "0.8125rem",
+    XS: "0.875rem",
+    SM: "0.9375rem",
+    MD: "1rem",
+    LG: "1.125rem",
+    XL: "1.25rem",
+    XXL: "1.5rem",
+  },
+  COLORS: {
+    C1: "#AD1FEA",
+    C2: "#4661E6",
+    C3: "#4661E6",
+    C4: "#FFFFFF",
+    C5: "#F2F4FF",
+    C6: "#F7F8FD",
+    C7: "#3A4374",
+    C8: "#647196",
+    C9: "#F49F85",
+    C10: "#62BCFA",
+  },
+};
